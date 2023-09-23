90bff3cf8db5372bcedc6ca1df7952b24debbb24
lilados <nefaroxispl@gmail.com>
2023-09-23 15:34:48 +0200

    Commit

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,47 +7,92 @@ namespace Profanity;
 
 public class Game1 : Game
 {
+    private const int ScreenWidth = 1920, ScreenHeight = 1080;
+    private GraphicsDevice gpu;
+    private SpriteFont font;
+    public static int screenW, screenH;
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    private Input inp;
+    private RenderTarget2D MainTarget;
+
+    private Rectangle desktopRect;
+    private Rectangle screenRect;
     public Game1()
     {
-        _graphics = new GraphicsDeviceManager(this);
+        int desktop_width = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 10;
+        int desktop_height = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height - 10;
+        _graphics = new GraphicsDeviceManager(this)
+        {
+            PreferredBackBufferWidth = desktop_width,
+            PreferredBackBufferHeight = desktop_height,
+            IsFullScreen = false, PreferredDepthStencilFormat = DepthFormat.None,
+            GraphicsProfile = GraphicsProfile.HiDef
+        };
+
+        Window.IsBorderless = true;
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
     }
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
+        gpu = GraphicsDevice;
+        PresentationParameters pp = gpu.PresentationParameters;
+        _spriteBatch = new SpriteBatch(gpu);
+        MainTarget = new RenderTarget2D(gpu, ScreenWidth, ScreenHeight, false, pp.BackBufferFormat, DepthFormat.Depth24);
+        screenW = MainTarget.Width;
+        screenH = MainTarget.Height;
+        desktopRect = new Rectangle(0, 0, pp.BackBufferWidth, pp.BackBufferHeight);
+        screenRect = new Rectangle(0, 0, screenW, screenH);
 
+        inp = new Input(pp, MainTarget);
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        // TODO: use this.Content to load your game content here
+        font = Content.Load<SpriteFont>("Font");
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-            Keyboard.GetState().IsKeyDown(Keys.Escape))
+        inp.Update();
+        if (inp.KeyDown(Keys.Escape))
             Exit();
-
-        // TODO: Add your update logic here
 
         base.Update(gameTime);
     }
-
+    private void Set3DStates()
+    {
+        gpu.BlendState = BlendState.NonPremultiplied; gpu.DepthStencilState = DepthStencilState.Default;
+        if (gpu.RasterizerState.CullMode == CullMode.None)
+        {
+            RasterizerState rs = new RasterizerState { CullMode = CullMode.CullCounterClockwiseFace };
+            gpu.RasterizerState = rs;
+        }
+    }
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        gpu.SetRenderTarget(MainTarget);
 
-        // TODO: Add your drawing code here
-
+        Set3DStates();
+        gpu.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.Black, 1.0f, 0);
+        
+        //Render Scene Objects
+        
+        
+        
+        //Draw mainTarget To BackBuffer
+        gpu.SetRenderTarget(null);
+        _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Opaque, SamplerState.LinearWrap, DepthStencilState.None,
+            RasterizerState.CullNone);
+        _spriteBatch.Draw(MainTarget, desktopRect, Color.White);
+        _spriteBatch.End();
+        
         base.Draw(gameTime);
     }
+
+    
 }
---
+++ Input.cs
@@ -0,0 +1,83 @@
+using System.Runtime.CompilerServices;
+using System.Security;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Profanity;
+class Input
+{
+    public const ButtonState ButtonUp = ButtonState.Released;
+    public const ButtonState ButtonDown = ButtonState.Pressed;
+    
+    //Keyboard
+
+    public KeyboardState kb, pkb;
+    public bool shift_down, ctrl_down, alt_down, shift_press, ctrl_press, alt_press;
+    public bool prev_shift_down, prev_ctrl_down, prev_alt_down;
+    
+    //Mouse
+    public MouseState ms, pms;
+    public bool leftClick, middleClick, rightClick, leftDown, middleDown, rightDown;
+    public int mouseX, mouseY;
+    public Vector2 mosPos;
+    public Point mp;
+
+    private float screenScaleX, screenScaleY;
+
+    public Input(PresentationParameters pp, RenderTarget2D target)
+    {
+        screenScaleX = 1.0f / ((float)pp.BackBufferWidth / target.Width);
+        screenScaleY = 1.0f / ((float)pp.BackBufferHeight / target.Height);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool KeyPress(Keys k)
+    {
+        if (kb.IsKeyDown(k) && pkb.IsKeyUp(k)) return true;
+        else return false;
+    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool KeyDown(Keys k)
+    {
+        if (kb.IsKeyDown(k)) return true;
+        else return false;
+    }
+
+    public void Update()
+    {
+        prev_alt_down = alt_down;
+        prev_shift_down = shift_down;
+        prev_ctrl_down = ctrl_down;
+        pkb = kb;
+        pms = ms;
+        kb = Keyboard.GetState();
+        ms = Mouse.GetState();
+        
+        //Keyboard 
+        shift_down = shift_press = ctrl_down = ctrl_press = alt_down = alt_press = false;
+        if (kb.IsKeyDown(Keys.LeftShift) || kb.IsKeyDown(Keys.RightShift)) shift_down = true;
+        if (kb.IsKeyDown(Keys.LeftControl) || kb.IsKeyDown(Keys.RightControl)) ctrl_down = true;
+        if (kb.IsKeyDown(Keys.LeftAlt) || kb.IsKeyDown(Keys.RightAlt)) alt_down = true;
+        if (shift_down && !prev_shift_down) shift_press = true;
+        if (ctrl_down && !prev_ctrl_down) ctrl_press = true;
+        if (alt_down && !prev_alt_down) alt_press = true;
+        
+        
+        //Mouse
+        mosPos = new Vector2(ms.Position.X * screenScaleX, ms.Position.Y * screenScaleY);
+        mouseX = (int)mosPos.X;
+        mouseY = (int)mosPos.Y;
+        mp = new Point(mouseX, mouseY);
+        if (ms.LeftButton == ButtonDown) leftDown = true;
+        if (ms.MiddleButton == ButtonDown) middleDown = true;
+        if (ms.RightButton == ButtonDown) rightDown = true;
+        if (leftDown && pms.LeftButton == ButtonUp) leftClick = true;
+        if (middleDown && pms.MiddleButton == ButtonUp) middleClick = true;
+        if (middleDown && pms.MiddleButton == ButtonUp) middleClick = true;
+        if (rightDown && pms.RightButton == ButtonUp) rightClick = true;
+        
+
+
+    }
+}
